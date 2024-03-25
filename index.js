544fa454d8e48230b1b22202f20258af3d4d0f2d
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-03-25 18:49:44 +0530

    Create a Repo and add the models and views directories

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const mongoose = require('mongoose')
+const ejs = require('ejs')
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+
+const User = require('./models/user')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret:'notagoodsecret'}))
+
+mongoose.set('strict', true);
+mongoose.connect('mongodb://127.0.0.1:27017/loginDB');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id){
+        return res.redirect('/login')
+    }
+    next()
+}
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body
+    const hash = await bcrypt.hash(password, 12)
+    const user = new User({
+        username: username,
+        password: hash
+    })
+
+    user.save()
+    req.session.user_id = user._id
+    res.redirect('/')
+
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password} = req.body
+    const user = await User.findOne({ username: username })
+    const validPassword = await bcrypt.compare(password , user.password)
+
+    if(validPassword){
+        req.session.user_id = user._id
+        res.redirect('/secret')
+    } else {
+        res.send('Try Again')
+    }
+})
+
+app.post('/logout',(req,res) =>{
+    req.session.user_id = null
+    res.redirect('/login')
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
